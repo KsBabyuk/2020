e2e1fd94b855eb82b22b4650c147ad6a7e3d3275
KsBabyuk <ksbabyuk@gmail.com>
2021-07-19 09:52:14 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img className="logo" src={logo} alt="aplication-logo" />
+      <h3>Bienvenidos al Blockchain</h3>
+    </div>
+  );
+}
+
+export default App;
